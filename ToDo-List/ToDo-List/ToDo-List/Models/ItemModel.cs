5fd6b8661a74a2ccbac0734c0ba350a1e04b3dd6
Jacek Garlicki <thepandaamail@gmail.com>
2020-12-26 22:08:30 +0100

    Wysyłam to samo jeszcze raz

---
+++ ToDo-List/ToDo-List/ToDo-List/Models/ItemModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDo_List.Models
+{
+	public class ItemModel
+	{
+		public int Id { get; set; }
+		public string Text { get; set; }
+	}
+}
--- ToDo-List/ToDo-List/ToDo-List/Views/MainPage.xaml.cs
+++ ToDo-List/ToDo-List/ToDo-List/Views/MainPage.xaml.cs
@@ -1,23 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using ToDo_List.Models;
 
 namespace ToDo_List
 {
 	public partial class MainPage : ContentPage
 	{
+		List<ItemModel> items = new List<ItemModel>();
 		public MainPage()
 		{
 			InitializeComponent();
 		}
-
 		private void Button_Clicked(object sender, EventArgs e)
 		{
-
+			items.Add(new ItemModel { Id = items.Count,Text = entry.Text });
 		}
 	}
 }
