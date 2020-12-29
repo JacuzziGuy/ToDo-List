271a520d1893b959137ba7fd602c2f74cbbab6ca
Jacek Garlicki <thepandaamail@gmail.com>
2020-12-29 21:42:22 +0100

    Dodany przycisk Usuń

--- ToDo-List/ToDo-List/ToDo-List/Views/MainPage.xaml.cs
+++ ToDo-List/ToDo-List/ToDo-List/Views/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using Xamarin.Forms;
 using ToDo_List.Models;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace ToDo_List.Views
 {
@@ -19,13 +20,11 @@ namespace ToDo_List.Views
 			itemsList.ItemsSource = Items;
 			itemsList.ItemSelected += ItemsList_ItemSelected;
 		}
-
 		private void ItemsList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
 		{
 			itemsList.SelectedItem = null;
 		}
-
-		private void Button_Clicked(object sender, EventArgs e)
+		private void SubmitClicked(object sender, EventArgs e)
 		{
 			if (entry.Text == "")
 			{
@@ -34,6 +33,19 @@ namespace ToDo_List.Views
 			}
 			Items.Add(new ItemModel { Id = Items.Count, Name = entry.Text });
 			entry.Text = "";
+		}
+		private void DeleteClicked(object sender, EventArgs e)
+		{
+			var button = (Button)sender;
+			int index = Items.FirstOrDefault(x => x.Id == (int)button.CommandParameter).Id;
+			//foreach (ItemModel x in Items)
+			//{
+			//	if (x.Id == (int)button.CommandParameter)
+			//	{
+			//		index = x.Id;
+			//	}
+			//}
+			Items.RemoveAt(index);
 		}
 	}
 }
